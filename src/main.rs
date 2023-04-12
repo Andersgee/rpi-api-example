ff128d11c9c109bd1cb66b52657aeed02e77c81e
andy <andersgee@gmail.com>
2023-04-12 16:38:13 +0200

    tidy

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,32 @@
-use actix::Actor;
-use actix::*;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use dotenv::dotenv;
-use serde::{Deserialize, Serialize};
 mod valve;
 
-#[derive(Serialize)]
-struct Info {
-    message: String,
-}
+use actix::{Actor, Addr};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Authquery {
+pub struct OpenvalveQuery {
     secret: String,
     ms: u64,
 }
 
-#[get("/toggle")]
-pub async fn toggle(
+#[get("/openvalve")]
+pub async fn openvalve(
     valve_addr: web::Data<Addr<valve::Valve>>,
-    query: web::Query<Authquery>,
+    query: web::Query<OpenvalveQuery>,
 ) -> impl Responder {
-    if query.secret != std::env::var("ADMIN_KEY").unwrap() {
-        HttpResponse::Forbidden().json(Info {
-            message: String::from("nope"),
-        })
-    } else {
-        let addr = valve_addr.get_ref();
-        let _res = addr.send(valve::ToggleValveMessage { ms: query.ms }).await;
-
-        HttpResponse::Ok().json(Info {
-            message: String::from("ok"),
-        })
-    }
-}
-
-#[get("/stuff2")]
-pub async fn stuff2(query: web::Query<Authquery>) -> impl Responder {
-    if query.secret != std::env::var("ADMIN_KEY").unwrap() {
-        HttpResponse::Forbidden().json(Info {
-            message: String::from("nope"),
-        })
-    } else {
-        HttpResponse::Ok().json(Info {
-            message: String::from("ok"),
-        })
-    }
-}
-#[get("/kek")]
-pub async fn kek(query: web::Query<Authquery>) -> impl Responder {
-    if query.secret != std::env::var("ADMIN_KEY").unwrap() {
-        HttpResponse::Forbidden().json(Info {
-            message: String::from("nope"),
-        })
-    } else {
-        HttpResponse::Ok().json(Info {
-            message: String::from("ok"),
-        })
+    let authed = query.secret != std::env::var("SECRET").unwrap();
+    match authed {
+        false => HttpResponse::Forbidden(),
+        true => {
+            let addr = valve_addr.get_ref();
+            let res = addr.send(valve::ToggleValveMessage { ms: query.ms }).await;
+            match res {
+                Ok(_) => HttpResponse::Ok(),
+                Err(_) => HttpResponse::InternalServerError(),
+            }
+        }
     }
 }
 
@@ -64,19 +34,14 @@ pub async fn kek(query: web::Query<Authquery>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     let api_port = std::env::var("API_PORT").expect("expected API_PORT to exists in environment");
-    let _admin_key =
-        std::env::var("ADMIN_KEY").expect("expected ADMIN_KEY to exists in environment");
-
+    let _secret = std::env::var("SECRET").expect("expected SECRET to exists in environment");
     let valve_addr = valve::Valve::new().start();
 
-    println!("listening on port {}", &api_port);
-
+    println!("server listening on port {}", &api_port);
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(valve_addr.clone()))
-            .service(stuff2)
-            .service(kek)
-            .service(toggle)
+            .service(openvalve)
     })
     .bind(format!("0.0.0.0:{}", api_port))?
     .run()
