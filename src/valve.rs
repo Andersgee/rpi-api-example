b02556d157535c92913e9909ca1dc0a40b103ae7
andy <andersgee@gmail.com>
2023-04-12 16:57:59 +0200

    tidy

--- src/valve.rs
+++ src/valve.rs
@@ -2,42 +2,48 @@ use actix::prelude::*;
 use rppal::gpio::{Gpio, OutputPin};
 use std::time::Duration;
 
-// Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const GPIO_LED: u8 = 18;
-
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct ToggleValveMessage {
-    pub ms: u64,
-}
-
 pub struct Valve {
     pin: OutputPin,
 }
 
 impl Valve {
     pub fn new() -> Self {
+        // Gpio uses BCM pin numbering. for example: BCM GPIO 23 is tied to physical pin 16.
+        let pin_number: u8 = 18;
+
+        //for my particular relay: high=off, low=on, so initialize on high
         let pin = Gpio::new()
             .expect("expected gpio new to be fine")
-            .get(GPIO_LED)
-            .expect("expected to get GPIO_LED to be fine")
+            .get(pin_number)
+            .expect("expected to get pin_number to be fine")
             .into_output_high();
         Self { pin }
     }
 
     fn toggle(&mut self, ctx: &mut Context<Self>, ms: u64) {
-        self.pin.set_low();
-        //println!("toggle called.");
+        self.pin.set_low(); //open
         ctx.run_later(Duration::from_millis(ms), |act, _ctx| {
-            act.pin.set_high();
-            //println!("run_later after {} ms", &ms);
+            act.pin.set_high(); //close
         });
     }
 }
 
+/**
+ * setup actor and message listener for Valve. send a message like this:
+ *
+ * ```rs
+ * let valve_addr = valve::Valve::new().start(); //start actor
+ * valve_addr.send(valve::ToggleValveMessage { ms: query.ms }) //send message to it
+ * ```
+ */
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ToggleValveMessage {
+    pub ms: u64,
+}
+
 impl Actor for Valve {
     type Context = Context<Self>;
-
     fn started(&mut self, _ctx: &mut Self::Context) {
         println!("actor for valve started")
     }
